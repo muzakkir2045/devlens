3b8dfa2a87acf52bb5c672fc1eb920ee0ccfb031
muzakkir2045 <muzakkir2045@gmail.com>
2026-03-17 17:29:52 +0530

    add headers

--- fetcher.py
+++ fetcher.py
@@ -1,31 +1,37 @@
-
+import os
+from dotenv import load_dotenv
 import requests
 import base64
 from pprint import pprint
+load_dotenv()
 
 url = input("Enter a github repo url : ")
 
 def extraction(url):
     lst = url.split('/')
-    owner = lst[3]
-    repo_name = lst[4]
+    owner = lst[3].strip()
+    repo_name = lst[4].strip()
     return owner,repo_name
 
-def get_repo_files(owner, repo_name):
 
+def get_repo_files(owner, repo_name):
+    headers = {
+        "Authorization" : f"Bearer {os.getenv("token")}",
+        "Accept": "application/vnd.github+json"
+    }
     repo_url = f"https://api.github.com/repos/{owner}/{repo_name}/git/trees/main?recursive=1"
-    response = requests.get(repo_url)
-    content = response.json()
+    response = requests.get(repo_url, headers=headers)
+    repo_content = response.json()
     # pprint(content)
 
-    files = [ item['path'] for item in content['tree'] if item['type'] == 'blob']
+    files = [ item['path'] for item in repo_content['tree'] if item['type'] == 'blob']
     print(files)
 
     content = {}
 
     for file_path in files:
         content_url = f"https://api.github.com/repos/{owner}/{repo_name}/contents/{file_path}"
-        response = requests.get(content_url)
+        response = requests.get(content_url, headers=headers)
         decoded = base64.b64decode(response.json()['content']).decode('utf-8')
         content[file_path] = decoded
     
