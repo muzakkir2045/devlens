1b5da6f8aeac2791c5b2cc6f78a8a375aab363f8
muzakkir2045 <muzakkir2045@gmail.com>
2026-03-26 15:20:05 +0530

    Add context generator and result aggregator

--- processor.py
+++ processor.py
@@ -1,7 +1,7 @@
 
 import os
 from fetcher import get_repo_files
-from pprint import pprint
+from more_itertools import unique_everseen
 
 data = get_repo_files()
 
@@ -43,16 +43,12 @@ def detect_languages():
 has_tests = any('test' in path.lower() for path in file_map().keys())
 
 def framework():
-
     frameworks = []
     for path in file_map().keys():
         fwk = os.path.basename(path)
         if fwk in FRAMEWORK_MAP:
             frameworks.append(FRAMEWORK_MAP[fwk])
     return frameworks if frameworks else None
-
-
-
 
 
 def entry():
@@ -78,4 +74,50 @@ def find_readme():
         if path.split('/')[-1] == 'README.md':
             return path
     return None
+
+
+def build_context():
+    MAX_CHARS = 30000
+    context = ""
+    all_files = []
+    fm = file_map()
+    readme = find_readme()
+    entry_files = entry()
+    paths = [key for key in file_map().keys()]
+    
+    if readme != None:
+        all_files.append(readme)
+    if entry_files != None:
+        all_files.extend(entry_files)
+
+    all_files.extend(paths)
+    all_files = list(unique_everseen(all_files))
+
+    for path in all_files:
+        content = fm.get(path,"")
+        ext = os.path.splitext(path)[1].replace('.', '')
+        chunk = f"File: {path}\n```{ext}\n{content}\n```\n\n"
+        
+        if len(context) + len(chunk) > MAX_CHARS:
+            break  # stop here — context is full enough
+        context += chunk
+    return context
+
+def processed():
+    gist = dict(list(data.items())[:3])
+    
+    gist['file_map'] = file_map()
+    gist['file_paths'] = [key for key in file_map().keys()]
+    gist['tech_stack'] = {
+        "languages" : detect_languages(),
+        "frameworks" : framework(),
+        "has_tests" : has_tests
+    }
+    gist['key_files'] = {
+        "entry_points" : entry(),
+        "config" : config(),
+        "readme" : find_readme()
+    }
+    gist['context'] = build_context()
+    return gist
 
